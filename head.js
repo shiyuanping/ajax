2c4a5e59fbbdab7040b6a5cc970694e169da6e1e
syp <925356588@qq.com>
2019-07-17 10:51:38 +0800

    post

---
+++ head.js
@@ -0,0 +1,8 @@
+document.writeln("<ul>");
+document.writeln("    <li><a href=\'./login.html\'>用户登录</a></li>");
+document.writeln("    <li><a href=\'./post.html\'>新增公告</a></li>");
+document.writeln("    <li><a href=\'./get.html\'>获取公告信息</a></li>");
+document.writeln("    <li><a href=\'./put.html\'>编辑公告信息</a></li>");
+document.writeln("    <li><a href=\'./delete.html\'>删除</a></li>");
+document.writeln("    <li><a href=\'./all.html\'>完整的增删改查</a></li> ");
+document.writeln("</ul>");
